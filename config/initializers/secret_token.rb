f13d34cdb824e1e1bea1b436301f394c598f4d58
Adam Fernandez <adampfernandez.com>
2012-04-24 23:28:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ringtack::Application.config.secret_token = 'baaea4229b02dd2aa0c213cf8ac536876522fc3095ffc3311eb8802546c4343a5f9b1412f85f4a24001c779b1b2d8dc4b7d74567c2a0b6a7c9e570921ca683e1'
