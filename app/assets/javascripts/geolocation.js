a72931d1efe2ab02839a7200d3eeb2dade9c64c7
Adam Fernandez <adampfernandez.com>
2012-08-28 20:37:58 -0400

    Quick geolocation change to js

--- app/assets/javascripts/geolocation.js
+++ app/assets/javascripts/geolocation.js
@@ -2,6 +2,6 @@ navigator.geolocation.getCurrentPosition(function(location) {
   console.log(location);
   $("#ring_latitude").val(location.coords.latitude);
   $("#ring_longitude").val(location.coords.longitude);
-  $("#session_latitude").val(location.coords.latitude);
-  $("#session_longitude").val(location.coords.longitude);
+  $("#latitude").val(location.coords.latitude);
+  $("#longitude").val(location.coords.longitude);
 })
