5ea8f76e08806abb22bba342bc1bd2c3901c1e45
Adam Fernandez <adampfernandez.com>
2012-09-10 01:40:14 -0400

    Updated ring sessions controller for nil case

--- app/controllers/ring_sessions_controller.rb
+++ app/controllers/ring_sessions_controller.rb
@@ -3,7 +3,10 @@ class RingSessionsController < ApplicationController
   def create
     if @insiderings = Ring.inside(params[:latitude], params[:longitude])
       session[:ring_session] = @insiderings.map(&:id).join(",")
-      redirect_to rings_path 
+      redirect_to rings_path
+    elsif
+      @insiderings = nil
+      "It appears no rings have been created here yet.  Why not create one now?" 
     else
       redirect_to root_path, :notice => "It appears we were unable to determine your location.
       Please make sure you are allowing your browser access to your location."
