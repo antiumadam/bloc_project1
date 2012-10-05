e07685033555096f45a7efe209a96208eedcab94
Adam Fernandez <adampfernandez.com>
2012-10-04 23:39:34 -0400

    Added google maps to rings

--- app/controllers/ring_sessions_controller.rb
+++ app/controllers/ring_sessions_controller.rb
@@ -4,12 +4,7 @@ class RingSessionsController < ApplicationController
     if @insiderings = Ring.inside(params[:latitude], params[:longitude])
       session[:ring_session] = @insiderings.map(&:id).join(",")
       redirect_to rings_path
-    elsif
-      @insiderings = nil
-      "It appears no rings have been created here yet.  Why not create one now?" 
-    else
-      redirect_to root_path, :notice => "It appears we were unable to determine your location.
-      Please make sure you are allowing your browser access to your location."
+
     end
   end
   
